9255901a4a6daae5c7afd0d4a4a22afcbfb9188d
opmind <turchaninov@infsys.ru>
2024-05-17 12:42:12 +0300

    - скрипт сборки заменен на node script [SEM-5743]: - исправлена верстка панелей

--- build.sh
+++
@@ -1,7 +0,0 @@
-export NODE_OPTIONS=--openssl-legacy-provider
-rm -rf ./dist
-webpack --config webpack.config.js
-rm ./dist/*.js
-cp -R ./src ./dist/components-web/src
-cp ./package.json ./dist/components-web/package.json
-node update_full.js
